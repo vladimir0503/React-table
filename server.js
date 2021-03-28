4cd59e13f1f185a430feca8f096ae15035281c59
Vladimir <vovan0503@mail.ru>
2021-03-28 21:14:25 +0300

    Fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 const port = Number(process.env.PORT || 3001);
 server.use(middlewares);
